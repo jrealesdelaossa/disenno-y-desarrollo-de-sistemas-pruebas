0f40c3f36c46628ed15a3a9d9444ab222cf1689d
Osdad <113603680+Osdad@users.noreply.github.com>
2023-07-13 17:18:04 -0500

    new commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as moment from 'moment';
+import moment from 'moment';
 
 @Controller()
 export class AppController {
